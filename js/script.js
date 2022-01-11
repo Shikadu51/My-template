1215b32bfc4a812bdaa3bf82c287e3423425e3fb
sebastien reveleau <74558866+Shikadu51@users.noreply.github.com>
2022-01-11 15:48:18 +0100

    Update script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const message = "Merci de nous avoir contacté.";
-
-document.getElementById("contactForm");
-addEventListener("submit", function (event) {
-  event.preventDefault();
-  alert(message);
-});
